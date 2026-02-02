65e5f0a1ef9667ff9ad3aa34e492b4e5889a8b91
Jorge Vera Zambrano <125833333+JorgeVeraz23@users.noreply.github.com>
2026-02-02 04:14:03 -0500

    completo

--- TicketsAPI/TicketsAPI/obj/Debug/net8.0/TicketsAPI.AssemblyInfo.cs
+++ TicketsAPI/TicketsAPI/obj/Debug/net8.0/TicketsAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793ae8bf16827168b17804571bbc0b3fc59dcfc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42ee0fc82d7af03a0bb4ecfe80e82f1607dc447")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
